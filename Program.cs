301919f403761bfcb2662d5d9debbdc35c4fcb4b
Polyash14 <polyafire14@gmail.com>
2021-03-17 17:35:52 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Array_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Вывести элементы массива на экран в обратном порядке.
+
+
+          
+
+            int[] arr = { 3, 5, 7, 12, 41, 2, 6 };
+
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                Console.WriteLine(arr[i]);
+            }
+
+                for ( int i = arr.Length - 1; i >= 0; i--)
+                {
+                    
+
+                    
+
+                    Console.WriteLine(arr[i]);
+
+                }
+            
+        }
+    }
+}
